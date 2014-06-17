36688ee88250a40dbeff800743ec930be6f07ff0
unknown <kyle.ouellette@CPUKOUELLETTE.corp.ecw.local>
2014-06-17 16:14:43 -0400

    file dialog now returns path

--- VBAutoRegister/main.cpp
+++ VBAutoRegister/main.cpp
@@ -1,19 +1,50 @@
+#include <fstream>
 #include <Windows.h>
+#include "main.h"
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
+
+	char fileToOpen[MAX_PATH] = { 0 };
+	getVbpFileLocation(fileToOpen);
+
+	std::fstream fileStream;
+	fileStream.open(fileToOpen, std::fstream::in);
+
+	char lineBuffer[255] = { 0 };
+	while (!fileStream.eof()) {
+		fileStream.getline(lineBuffer, sizeof(lineBuffer));
+
+		std::string line = std::string(lineBuffer);
+
+		if (isObjectReference(line))
+			MessageBox(NULL, line.c_str(), "", MB_OK);
+		else if (isReference(line))
+			MessageBox(NULL, line.c_str(), "", MB_OK);
+
+	}
 
 	return 0;
 }
 
-//TODO: return filepath
-void getVbpFileLocation(){
+bool isObjectReference(std::string line) {
+	return strncmp(line.c_str(), "Object=", std::string("Object=").length()) == 0;
+}
 
-	OPENFILENAME openFilename = {};
+bool isReference(std::string line) {
+	return strncmp(line.c_str(), "Reference=", std::string("Reference=").length()) == 0;
+}
 
-	openFilename.lStructSize = sizeof(OPENFILENAME);
+void getVbpFileLocation(LPSTR fileToOpen) {
+
+	OPENFILENAME openFilename;
+	ZeroMemory(&openFilename, sizeof(openFilename));
+
+	openFilename.lStructSize = sizeof(openFilename);
 	openFilename.lpstrFilter = "Visual Basic Project File\0*.VBP\0\0";
 	openFilename.lpstrFile = "\0";
 	openFilename.nMaxFile = MAX_PATH;
+	openFilename.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+	openFilename.lpstrFile = fileToOpen;
 
 	GetOpenFileName(&openFilename);
 }
--- VBAutoRegister/main.h
+++ VBAutoRegister/main.h
@@ -1,1 +1,3 @@
-void getVbpFileLocation();
+void getVbpFileLocation(LPSTR fileToOpen);
+bool isObjectReference(std::string line);
+bool isReference(std::string line);
